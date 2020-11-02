f1ab11f59d846dfc328c729ac85e8763d3bd6e13
Marco <marcovisser6@gmail.com>
2020-11-02 14:56:08 +0200

    Some changes & fixes.

--- server.js
+++ server.js
@@ -1,23 +1,34 @@
 //Open Weather Map API Key: 2af550162ed8871fabf0fed4afe2a6e6
+// API Call: pro.openweathermap.org/data/2.5/forecast/hourly?zip={zip code},{country code}&appid={API key}
+
 //Load all required modules.
 const express = require('express'); 
+var path = require('path');
 
 //Create an object for the express module.
 const app = express();
 
-// The first route
+//Third party middleware.
+app.use(express.urlencoded({extended:true}));
+
+// The first route.
 app.get('/', function (req, res) {
-    res.sendFile('./pages/index.html',{root:__dirname});
+    res.sendFile(path.join(__dirname + '/pages/index.html'));
 });
 
-// The second route
+//Receive location from front-end.
+app.post('/getLocation', function (req, res) {
+    res.send(req.body);
+});
+
+// The second route.
 app.get('/about', function (req, res) {
-    res.send('Second Route',{root:__dirname});
+    res.sendFile(path.join(__dirname + '/pages/about.html'));
 });
 
-//Default 404 output.
+//Default 404 route.
 app.use(function (req, res) {
-    res.sendFile('./pages/404.html',{root:__dirname});
+    res.sendFile(path.join(__dirname + '/pages/404.html'));
 })
 
 // Listen to port 5000
